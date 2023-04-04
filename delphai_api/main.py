a6c19dfe54c787a104b2c865f788da2895ecd387
Anton Ryzhov <anton@delphai.com>
2023-04-04 13:21:26 +0200

    Link the authentication document

--- delphai_api/main.py
+++ delphai_api/main.py
@@ -4,8 +4,22 @@ from urllib.parse import urlsplit
 from .companies import routes as companies_routes
 from .utils import include_responses
 
+DESCRIPTION = """
+<p>
+    This is delphai's API documentation and developer's portal.
+    You need to have valid credentials to make calls.
+    If you already have them, just click <b>Authorize</b> button to retrieve the token.
+</p>
+<p>
+    Check out
+    <a href="https://github.com/delphai/docs/blob/master/auth-flow.md">this page</a>
+    for more information about authentication flow and API client examples.
+</p>
+"""
+
 app = FastAPI(
     title="delphai API",
+    description=DESCRIPTION,
     docs_url="/",
     redoc_url=None,
     swagger_ui_oauth2_redirect_url="/oauth2-redirect",
