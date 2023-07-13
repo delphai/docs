2666f79d6541bcb278f56e73973f4984ed501e5b
leofj <leonardo@delphai.com>
2023-07-13 15:28:46 +0200

    chore: formatting

--- delphai_api/types.py
+++ delphai_api/types.py
@@ -46,7 +46,7 @@ HeadquartersField = subfields(
     "headquarters",
     Optional[str],
     ["continent", "country", "state", "city"],
-    description="Headquarters filter. Available options: continent, country, state, city.",
+    description="Headquarters filter. Available options: continent, country, state, city.",  # noqa
     example="[country]=Germany",
 )
 
